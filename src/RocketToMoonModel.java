e051f93f4f379e0d888531102247ca20bf2cc255
Orion Layton <orionlayton@mac.com>
2016-10-04 17:50:23 -0600

    Working on rocket

--- src/RocketToMoonModel.java
+++ src/RocketToMoonModel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Event;
 
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
@@ -10,21 +11,21 @@ public class RocketToMoonModel extends GraphicsProgram{
 	private static final double ROCKET_WIDTH = 2;
 	private static final double ROCKET_HEIGHT = 8;
 	//Size XL rocket for viewing clarity.
-	private static final double EARTH_SIZE = 15;
+	private static final double EARTH_RADIUS_IN_MILES = 3959;
+	//scaled to fit and doubled for its diameter:
+	private static final double EARTH_SIZE = EARTH_RADIUS_IN_MILES*.004;
 	private static final double EARTH_LEVEL = FRAME_HEIGHT/2-EARTH_SIZE/2;
 	private static final double MOON_SIZE = EARTH_SIZE*.27;
 	//The moon is approx. 27% the size of the earth.
 	private static final double EARTH_RADII = EARTH_SIZE/2;
 	private static final double MEAN_LUNAR_DISTANCE = EARTH_RADII*60.32;
 	//The distance changes over the course of the orbit but this is the *mean*, relative to Earth's radius
-	private static final double ROCKET_TOP_X = FRAME_WIDTH/2+ROCKET_WIDTH/2-2;
+	private static final double ROCKET_TOP_X = FRAME_WIDTH/2;
 	private static final double ROCKET_TOP_Y = EARTH_LEVEL-ROCKET_HEIGHT*1.5;
 	private static final int SEGMENTS = 3;
 	private static final double LUNAR_ORBIT_DAYS = 27;
 	private static final double LUNAR_MOVEMENT_PER_DAY = 360/LUNAR_ORBIT_DAYS;
 	private static final double LUNAR_MOVEMENT_PER_HOUR = 360/(LUNAR_ORBIT_DAYS*24);
-	
-
 	
 	public void init(){
 		this.setSize(FRAME_WIDTH,FRAME_HEIGHT);
@@ -35,17 +36,27 @@ public class RocketToMoonModel extends GraphicsProgram{
 		add(earth);
 		Rocket rocket = new Rocket(ROCKET_TOP_X, ROCKET_TOP_Y, ROCKET_WIDTH, ROCKET_HEIGHT, EARTH_LEVEL, SEGMENTS);
 		add(rocket);
-		Moon moon = new Moon(FRAME_WIDTH/2-EARTH_SIZE/2, FRAME_HEIGHT/2-MEAN_LUNAR_DISTANCE, MOON_SIZE, MOON_SIZE);
+		Moon moon = new Moon(FRAME_WIDTH/2, FRAME_HEIGHT/2-MEAN_LUNAR_DISTANCE, MOON_SIZE, MOON_SIZE);
 		add(moon);
 		lunarOrbit(moon);
-		rocketLaunch(rocket);
+		
 	}
-	public void rocketLaunch(Rocket rocket){
+	public void rocketLaunch(Rocket rocket, Moon moon){
+		double y = FRAME_HEIGHT/2;
+		double moonIntersect = FRAME_HEIGHT/2-MEAN_LUNAR_DISTANCE;
+		while (y > moonIntersect){
+			rocket.move(0, -1);
+			pause(5);
+		}
+	}
+	public void LaunchWindow(Event e){
 		
 	}
 	public void lunarOrbit(Moon moon){
+		//moon.addActionListener(moon);
 		double angle = 0.0;
 		double angleStepSize = LUNAR_MOVEMENT_PER_HOUR*Math.PI/180;
+		double noonInRadians = 270 * Math.PI / 180;
 		while (angle < 2 * Math.PI){
 			angle += angleStepSize;
 			moon.setLocation(MEAN_LUNAR_DISTANCE*Math.cos(angle)+FRAME_WIDTH/2, MEAN_LUNAR_DISTANCE*Math.sin(angle)+FRAME_HEIGHT/2);
